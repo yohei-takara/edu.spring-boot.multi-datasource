a118955b63145c661e18a2e305443656521bf4f8
yohei-takara <yohei.takara@cacco.co.jp>
2018-01-04 18:11:53 +0900

    Docker mysql initialize DDL 追加 & 修正

--- .docker/mysql/initialize/1_create_table.sql
+++ .docker/mysql/initialize/1_create_table.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `human`;
 
 CREATE TABLE `human` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT '',
   `age` int(3) DEFAULT 0,
   `sex` tinyint(1) DEFAULT TRUE,
---
+++ .docker/mysql/initialize/2_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO multi_datasource.human (name, age, sex) VALUES ('honda', 50, 1);
+INSERT INTO multi_datasource.human (name, age, sex) VALUES ('yamada', 10, 1);
+INSERT INTO multi_datasource.human (name, age, sex) VALUES ('suzuki', 200, 0);
+INSERT INTO multi_datasource.human (name, age, sex) VALUES ('sato', 5, 0);
