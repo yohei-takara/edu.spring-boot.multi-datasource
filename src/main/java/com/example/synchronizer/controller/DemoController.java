d350b70f833978b84da5e97417ff55e6a3176e4f
yohei-takara <yohei.takara@cacco.co.jp>
2018-01-04 16:38:17 +0900

    Tyhmeleaf + mybatis の 実行サンプル

--- src/main/java/com/example/synchronizer/controller/DemoController.java
+++ src/main/java/com/example/synchronizer/controller/DemoController.java
@@ -1,17 +1,28 @@
 package com.example.synchronizer.controller;
 
+import com.example.synchronizer.repository.HumanRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
 
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String hello() {
+    private HumanRepository humanRepository;
 
-        return "Hello world";
+    DemoController(HumanRepository humanRepository) {
+        this.humanRepository = humanRepository;
+    }
+
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+
+        int cnt = humanRepository.count();
+
+        model.addAttribute("count", String.valueOf(cnt));
+        model.addAttribute("message", "Hello world");
+
+        return "hello";
     }
 
 }
