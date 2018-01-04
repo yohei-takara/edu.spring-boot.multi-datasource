dfff5a05d8ff2bfa2fadd7a18f433af15ee834d6
yohei-takara <yohei.takara@cacco.co.jp>
2018-01-04 18:13:15 +0900

    TyhmeleafでのObject を渡す & foreach サンプル実装

--- src/main/java/com/example/synchronizer/controller/DemoController.java
+++ src/main/java/com/example/synchronizer/controller/DemoController.java
@@ -1,9 +1,12 @@
 package com.example.synchronizer.controller;
 
+import com.example.synchronizer.model.HumanModel;
 import com.example.synchronizer.repository.HumanRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class DemoController {
@@ -18,9 +21,16 @@ public class DemoController {
     public String hello(Model model) {
 
         int cnt = humanRepository.count();
+        HumanModel human = humanRepository.fetchById(1);
+        List<HumanModel> humans = humanRepository.fetchAll();
 
-        model.addAttribute("count", String.valueOf(cnt));
+
+        model.addAttribute("count", cnt);
         model.addAttribute("message", "Hello world");
+        model.addAttribute("name", human.getNameName());
+        model.addAttribute("age", human.getAgeAge());
+        model.addAttribute("sex", human.isSex());
+        model.addAttribute("humans", humans);
 
         return "hello";
     }
